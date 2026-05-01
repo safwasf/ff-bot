4438d7e4953fb64e08a52f9ba0fe17688c7f5061
safwasf <hamikimar@gmail.com>
2026-05-01 16:14:16 +0530

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -8,6 +8,11 @@ class KeepAliveHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(b"Bot is alive!")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
     
     def log_message(self, format, *args):
         pass # Disable logging to keep console clean
